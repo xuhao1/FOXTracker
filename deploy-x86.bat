55e383393473178c5eac8abdcd777bb9c0960eaf
xuhao1 <xuhao3e8@gmail.com>
2020-10-13 17:57:23 +0800

    Detele useless files

--- deploy-x86.bat
+++ deploy-x86.bat
@@ -3,5 +3,7 @@ mkdir Release.x86
 mkdir Release.x86\\assets
 copy ..\\build-FlightAgentX-Desktop_Qt_5_14_2_MSVC2017_32bit-Release\\release\\FlightAgentX.exe .\\Release.x86
 copy  .\\assets .\\Release.x86\assets
+copy  .\\config.yaml .\\Release.x86\config.yaml
+
 cd Release.x86
 windeployqt FlightAgentX.exe
--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,7 @@ mkdir Release
 mkdir Release\\assets
 copy ..\\build-FlightAgentX-Desktop_Qt_5_14_2_MSVC2017_64bit-Release\\release\\FlightAgentX.exe .\\Release
 copy  .\\assets .\\Release\assets
+copy  .\\config.yaml .\\Release.x86\config.yaml
+
 cd Release
 windeployqt FlightAgentX.exe
